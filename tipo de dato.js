82ee17cf3765b41bb87029f78fb94bcf65e4f7fa
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-02-04 20:49:13 -0600

    Modulo3, Proyecto1:Tipos de datos

---
+++ tipo de dato.js
@@ -0,0 +1,55 @@
+//Este es un comentario en JS
+
+//Console.log es una función que se utiliza para imprimir en consola
+
+console.log("Hola mundo");
+
+//Declaración de Variables
+//La semantica es importante, ya que corresponde al significado de las palabras
+var nombre = "Sergio";
+var edad = 27;
+var sueldo = 12.45;
+var casado = false;
+var hombre = true;
+var hijos = null;
+var dato;
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casado));
+console.log(typeof(hombre));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+/* Sí ponemos un typeof de null dira que es un objeto, esto es un error que no se ha 
+corregido por problemas de compatibilidad.*/
+
+console.log(10 * "Sergio");
+//NaN (Not at Number)
+
+//Modulo III, Proyecto 1: Ejercicio de tipos de datos
+
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(''));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+
+console.log(typeof("Estado de México"));
+console.log(typeof(13.6));
+console.log(typeof(5<4));
+console.log(typeof(-0.15));
+console.log(typeof(1>0));
+
+var velocidadDelaLuz = 300000000;
+var velocidadDelSonido = 343;
+var comparacionEntreVelocidades = velocidadDelSonido<velocidadDelaLuz;
+
+console.log(typeof(velocidadDelaLuz));
+console.log(typeof(velocidadDelSonido));
+console.log(typeof(comparacionEntreVelocidades));
