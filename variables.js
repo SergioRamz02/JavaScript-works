a9e013f576812776ec0c44b5dbd9eff4637c33c0
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-02-06 18:58:56 -0600

    Agregando variables.js

---
+++ variables.js
@@ -0,0 +1,26 @@
+//Ejemplos en clase 2
+
+//var-> Es una forma antigua de declarar variables
+
+var nombre = "Sergio";
+var nombre = "Román";
+var nombre = "Victor";
+
+console.log(nombre);
+
+/*let -> No permite redeclarar variables, lo que simplifica que 
+no pueda declara la misma variable dos veces*/
+//let tiene un scope de bloque. Un bloque es la 
+// información dentro de la variable
+
+let apellido1 = "Gutierrez";
+let apellido2 = "López";
+
+/*let si permite cambiar el valor de la variable*/
+apellido1 = "Perez";
+console.log(apellido1);
+
+//const -> Valor constante, no se puede reasignar valor
+const Pi = 3.14159;
+
+
