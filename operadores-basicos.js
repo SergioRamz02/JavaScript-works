2f1d10b4e9f0edf53f4e94c1c680ae40a0bab7a2
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-02-06 19:44:09 -0600

    Agregando operadores-basicos.js

---
+++ operadores-basicos.js
@@ -0,0 +1,89 @@
+//Operadores básicos
+
+let valor1 = 27;
+let valor2 = 4;
+
+//Operaciones básicas
+
+let suma = valor1 + valor2;
+console.log(suma);
+
+let resta = valor1 - valor2;
+console.log(resta);
+
+let producto = valor1 * valor2;
+console.log(producto);
+
+let division = valor1 / valor2;
+console.log(division);
+
+let modulo = valor1 % valor2;
+console.log(modulo);
+
+let exponencial = valor1**valor2;
+console.log(exponencial);
+
+//Operadores de comparación
+
+let numero1 = 3; //tipo number
+let numero2 = "3"; //tipo string
+
+console.log(numero1 == numero2); //Comparación débil
+console.log(numero1 === numero2); //Comparación estricta
+
+//Operadores lógicos y de comparación
+console.log("Operadores lógicos y de comparación");
+
+console.log (10 > 4);
+console.log(2 < 1);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+//La respuesta es un boleano
+
+//Operador de diferencia
+console.log("Diferentes de");
+
+console.log(15 != 15); //False porque es el mismo valor
+
+//Operadores Lógicos
+console.log("Operadores Lógicos");
+
+//Operador &&
+console.log("AND: &&");
+
+//Para poder entrar a un antro
+let edad = 17;
+
+let tenerID = true;
+
+console.log(edad >= 18 && tenerID); 
+
+//Operador ||
+console.log("OR: ||");
+
+//Para aprender a programar
+let esMayorDeEdad = false;
+
+let esEstudiante = true;
+
+console.log(esMayorDeEdad || esEstudiante); 
+
+//Operador !
+console.log("NOT: !");
+
+let esFinDeSemana = true;
+
+console.log(!esFinDeSemana);
+
+//Sumas acumulativas
+
+let calificacion = 9;
+let calificacion1 = 10;
+let calificacion2 = 6;
+let calificacion3 = 7;
+let calificacion4 = 8;
+
+// += Nos permite realizar una suma acumulativa promedio += promedio + calificacion1
+
+
